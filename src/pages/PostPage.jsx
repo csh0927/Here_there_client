76a60e7ea828a2e3ce79d81385c67963ef1c1dcd
csh0927 <sung060927@gmail.com>
2024-06-24 10:16:27 +0900

    후기 작성 퍼블리싱 & 연동

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,80 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-import image from '@/assets/franch.svg';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import Header from '@/components/header/Header';
+import LoginHeader from '@/components/header/LoginHeader';
 
 const PostPage = () => {
-    const [interests] = useState(['바다', '산책로', '뷰', '맛집']);
-    const [liked, setLiked] = useState(false);
+    const { id } = useParams(); 
+    const token = localStorage.getItem("Key");
+    const [post, setPost] = useState({});
 
-    const comments = [
-        { author: '작성자1', text: '댓글 내용입니다. Lorem ipsum dolor sit amet.' },
-        { author: '작성자2', text: '댓글 내용입니다. Lorem ipsum dolor sit amet.' },
-        { author: '작성자3', text: '댓글 내용입니다. Lorem ipsum dolor sit amet.' },
-    ];
+    useEffect(() => {
+        getPostDetail();
+    }, [id]);
 
-    const touristSpot = {
-        name: '프랑스 파리',
-        address: 'Champ de Mars, 5 Av. Anatole France, 75007 Paris, 프랑스',
+    const getPostDetail = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8081/review/${id}`);
+            setPost(res.data);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     const handleAddressClick = () => {
         window.open(
-            `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(touristSpot.address)}`,
+            `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(post.address)}`,
             '_blank'
         );
     };
 
-    const handleLikeClick = () => {
-        setLiked(!liked);
-    };
-
     return (
         <div>
-            <Header />
+            {token ? <LoginHeader /> : <Header />}
             <PostContainer>
-                <PostTitle>게시글 제목</PostTitle>
-
-                <PostImage src={image} alt="게시글 사진" />
-
-                <LikeButton onClick={handleLikeClick}>{liked ? <FilledHeartIcon /> : <OutlineHeartIcon />}</LikeButton>
-
-                <PostContent>
-                    게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다.
-                    게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다.
-                    게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다.
-                    게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다.
-                    게시글 내용이 여기에 표시됩니다.게시글 내용이 여기에 표시됩니다. 게시글 내용이 여기에 표시됩니다.
-                    게시글 내용이 여기에 표시됩니다.
-                </PostContent>
-
+                <PostTitle>{post.title}</PostTitle>
+                <PostImage src={post.image} alt="게시글 사진" />
+                <PostContent>{post.content}</PostContent>
                 <TouristSpotInfo>
                     <h2>관광지 정보</h2>
                     <br />
-                    <p>{touristSpot.name}</p>
-                    <AddressLink onClick={handleAddressClick}>{touristSpot.address}</AddressLink>
-                    <p>관심사: {interests.join(', ')}</p>
+                    <p>{post.name}</p>
+                    <p>국가: {post.country}</p>
+                    <p>지역: {post.region}</p>
+                    <AddressLink onClick={handleAddressClick}>{post.address}</AddressLink>
+                    <p>관심사: {post.interests?.join(', ')}</p>
                 </TouristSpotInfo>
-
-                <CommentInputContainer>
-                    <CommentInput type="text" placeholder="댓글 작성" />
-                    <CommentButton>작성</CommentButton>
-                </CommentInputContainer>
-
-                <Comments>
-                    {comments.map((comment, index) => (
-                        <Comment key={index}>
-                            <CommentText>
-                                <strong>{comment.author}</strong>
-                                <br />
-                                {comment.text}
-                                <br />
-                                <br />
-                            </CommentText>
-                        </Comment>
-                    ))}
-                </Comments>
             </PostContainer>
         </div>
     );
@@ -118,45 +89,6 @@ const TouristSpotInfo = styled.div`
     margin-top: 60px;
 `;
 
-const CommentInputContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin: 0 auto 20px;
-    max-width: 400px;
-`;
-
-const CommentInput = styled.input`
-    flex: 1;
-    height: 40px;
-    padding: 0 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px 0 0 5px;
-`;
-
-const CommentButton = styled.button`
-    height: 40px;
-    padding: 0 20px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 0 5px 5px 0;
-    cursor: pointer;
-`;
-
-const Comments = styled.div`
-    border-top: 1px solid #ccc;
-    padding-top: 20px;
-`;
-
-const Comment = styled.div`
-    margin-bottom: 10px;
-`;
-
-const CommentText = styled.p`
-    margin: 0;
-    text-align: left;
-`;
-
 const AddressLink = styled.a`
     color: black;
     text-decoration: none;
@@ -171,23 +103,4 @@ const AddressLink = styled.a`
     &:hover {
         color: #007bff;
     }
-`;
-
-const LikeButton = styled.button`
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    position: absolute;
-    top: 50px;
-    right: 20px;
-`;
-
-const OutlineHeartIcon = styled(FaRegHeart)`
-    font-size: 24px;
-    color: red;
-`;
-
-const FilledHeartIcon = styled(FaHeart)`
-    font-size: 24px;
-    color: red;
 `;
