dd52be794c1148f6d4c369847d12b51b90b2a942
csh0927 <sung060927@gmail.com>
2024-06-24 10:16:10 +0900

    라우터 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "@/pages/MainPage";
 import LoginPage from "@/pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import MyPage from "./pages/MyPage";
+import SignupPage from "@/pages/SignupPage";
+import MyPage from "@/pages/MyPage";
+import MyReview from "@/pages/MyReview";
+import SearchPage from "@/pages/SearchPage";
+import PostPage from "@/pages/PostPage";
+import ReviewPage from "@/pages/ReviewPage";
+
 
 const Router = () => {
   return (
@@ -12,6 +17,10 @@ const Router = () => {
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/signup" element={<SignupPage />}/>
         <Route path="/mypage" element={<MyPage />}/>
+        <Route path="/my-reviews" element={<MyReview />}/>
+        <Route path="/search" element={<SearchPage />}/>
+        <Route path="/page/:id" element={<PostPage />}/>
+        <Route path="/review" element={<ReviewPage />}/>
       </Routes>
     </BrowserRouter>
   );
